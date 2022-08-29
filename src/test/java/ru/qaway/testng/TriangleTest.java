40d24259a6fce523e7a6caa7462dcda578938ccb
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-29 22:01:07 +0300

    TestNG course - second lesson

--- src/test/java/ru/qaway/testng/TriangleTest.java
+++ src/test/java/ru/qaway/testng/TriangleTest.java
@@ -5,28 +5,28 @@ import org.testng.annotations.Test;
 
 public class TriangleTest {
 
-    @Test(description = "Check input is triangle")
+    @Test(description = "Check input is triangle", groups = "triangle")
     public void testTriangle() {
         String result = Triangle.isTriangle("4 5 6");
         Assert.assertEquals(result, Triangle.TRIANGLE, "Input is triangle");
     }
 
     @Test(description = "Check input is equilateral triangle",
-            dependsOnMethods = "testTriangle")
+            groups = "triangle.spec")
     public void testEquilateralTriangle() {
         String result = Triangle.isTriangle("4 5 6");
         Assert.assertEquals(result, Triangle.EQUILATERAL, "Input is equilateral triangle");
     }
 
     @Test(description = "Check input is isosceles triangle",
-            dependsOnMethods = "testEquilateralTriangle", alwaysRun = true)
+            groups = "triangle.spec")
     public void testIsoscelesTriangle() {
         String result = Triangle.isTriangle("4 6 6");
         Assert.assertEquals(result, Triangle.ISOSCELES, "Input is isosceles triangle");
     }
 
     @Test(description = "Check input is not triangle",
-            dependsOnMethods = {"testEquilateralTriangle","testIsoscelesTriangle"})
+            dependsOnGroups = "triangle.*", alwaysRun = true)
     public void testNotTriangle() {
         String result = Triangle.isTriangle("4 5 20");
         Assert.assertEquals(result, Triangle.NOT_TRIANGLE, "Input is not triangle");
