ab6861c55bd55ac5bdb9b5bac69838bda60b62f2
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-29 21:58:54 +0300

    TestNG course - second lesson

--- src/test/java/ru/qaway/testng/TriangleTest.java
+++ src/test/java/ru/qaway/testng/TriangleTest.java
@@ -5,25 +5,28 @@ import org.testng.annotations.Test;
 
 public class TriangleTest {
 
-    @Test(description = "Check input is triangle", priority = 0)
+    @Test(description = "Check input is triangle")
     public void testTriangle() {
         String result = Triangle.isTriangle("4 5 6");
         Assert.assertEquals(result, Triangle.TRIANGLE, "Input is triangle");
     }
 
-    @Test(description = "Check input is equilateral triangle", priority = 1)
+    @Test(description = "Check input is equilateral triangle",
+            dependsOnMethods = "testTriangle")
     public void testEquilateralTriangle() {
-        String result = Triangle.isTriangle("4 4 4");
+        String result = Triangle.isTriangle("4 5 6");
         Assert.assertEquals(result, Triangle.EQUILATERAL, "Input is equilateral triangle");
     }
 
-    @Test(description = "Check input is isosceles triangle", priority = 2, enabled = false)
+    @Test(description = "Check input is isosceles triangle",
+            dependsOnMethods = "testEquilateralTriangle", alwaysRun = true)
     public void testIsoscelesTriangle() {
         String result = Triangle.isTriangle("4 6 6");
         Assert.assertEquals(result, Triangle.ISOSCELES, "Input is isosceles triangle");
     }
 
-    @Test(description = "Check input is not triangle", priority = 3)
+    @Test(description = "Check input is not triangle",
+            dependsOnMethods = {"testEquilateralTriangle","testIsoscelesTriangle"})
     public void testNotTriangle() {
         String result = Triangle.isTriangle("4 5 20");
         Assert.assertEquals(result, Triangle.NOT_TRIANGLE, "Input is not triangle");
