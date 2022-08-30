429111a4d54ce59e91e0bba9ccebfe4806d5badb
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-30 09:41:47 +0300

    TestNG course - fourth lesson

--- src/test/java/ru/qaway/testng/ParametersTest.java
+++ src/test/java/ru/qaway/testng/ParametersTest.java
@@ -1,35 +1,32 @@
 package ru.qaway.testng;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ParametersTest {
 
     @BeforeClass
     @Parameters({"browser", "users"})
-    public void beforeClass(String browser, Integer users) {
+    public void beforeClass(@Optional("ie") String browser, @Optional("10") Integer users) {
         System.out.printf("Next tests will run with params " +
                 "(browser=%s, users=%s)\n\n", browser, users);
     }
 
     @Test
     @Parameters("browser")
-    public void testLogin(String browser) {
+    public void testLogin(@Optional("ie") String browser) {
         System.out.printf("Check login with browser=%s\n", browser);
     }
 
     @Test
     @Parameters({"browser", "users"})
-    public void testConcurrentLogin(String browser, Integer users) {
+    public void testConcurrentLogin(@Optional("ie") String browser, @Optional("10") Integer users) {
         System.out.printf("Check concurrent login with params" +
                 "(browser=%s, users=%s)\n", browser, users);
     }
 
     @AfterClass
     @Parameters({"browser", "users"})
-    public void afterClass(String browser, Integer users) {
+    public void afterClass(@Optional("ie") String browser, @Optional("10") Integer users) {
         System.out.printf("\nPrevious tests have run with params" +
                 "(browser=%s, users=%s)\n\n", browser, users);
     }
