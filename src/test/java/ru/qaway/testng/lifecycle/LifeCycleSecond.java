03308b52cfeb3bac00156abc2c97738d9c62eb5b
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-30 15:32:40 +0300

    TestNG course - 5th lesson

--- src/test/java/ru/qaway/testng/lifecycle/LifeCycleSecond.java
+++ src/test/java/ru/qaway/testng/lifecycle/LifeCycleSecond.java
@@ -1,10 +1,17 @@
 package ru.qaway.testng.lifecycle;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
 
 import java.lang.reflect.Method;
 
 public class LifeCycleSecond extends LifeCycleTestBase {
+
+    @BeforeTest
+    public void beforeTest(XmlTest xmlTest) {
+        System.out.printf("\tBeforeTest (from child) : %s <%s>\n", getClass().getSimpleName(), xmlTest.getName());
+    }
 
     @Test
     public void LifeCycleTest1(Method method) {
