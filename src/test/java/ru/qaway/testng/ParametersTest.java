b62f9aff1e64ba7dd5ccb1b4294cf2eaa16f8545
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-30 09:40:28 +0300

    TestNG course - fourth lesson

--- src/test/java/ru/qaway/testng/ParametersTest.java
+++ src/test/java/ru/qaway/testng/ParametersTest.java
@@ -1,9 +1,18 @@
 package ru.qaway.testng;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParametersTest {
+
+    @BeforeClass
+    @Parameters({"browser", "users"})
+    public void beforeClass(String browser, Integer users) {
+        System.out.printf("Next tests will run with params " +
+                "(browser=%s, users=%s)\n\n", browser, users);
+    }
 
     @Test
     @Parameters("browser")
@@ -16,5 +25,12 @@ public class ParametersTest {
     public void testConcurrentLogin(String browser, Integer users) {
         System.out.printf("Check concurrent login with params" +
                 "(browser=%s, users=%s)\n", browser, users);
+    }
+
+    @AfterClass
+    @Parameters({"browser", "users"})
+    public void afterClass(String browser, Integer users) {
+        System.out.printf("\nPrevious tests have run with params" +
+                "(browser=%s, users=%s)\n\n", browser, users);
     }
 }
