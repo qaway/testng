04587b328895d3cf6470c07c8d5ba43065955176
kbarzakovsky <kbarzakovsky@nordigy.ru>
2022-08-30 09:44:31 +0300

    TestNG course - fourth lesson

--- src/test/java/ru/qaway/testng/TriangleTest.java
+++ src/test/java/ru/qaway/testng/TriangleTest.java
@@ -16,12 +16,20 @@ public class TriangleTest {
     public Object[][] triangle() {
         return new Object[][] {
                 { "4 4 4", Triangle.EQUILATERAL},
+                { "2147483647 2147483647 2147483647", Triangle.EQUILATERAL},
+                { "1 1 1", Triangle.EQUILATERAL},
 
+                { "5 4 4", Triangle.ISOSCELES},
                 { "4 5 4", Triangle.ISOSCELES},
-
-                { "4 5 6", Triangle.TRIANGLE},
+                { "4 4 5", Triangle.ISOSCELES},
+                { "5 2147483647 2147483647", Triangle.ISOSCELES},
+                { "2147483647 5 2147483647", Triangle.ISOSCELES},
+                { "2147483647 2147483647 5", Triangle.ISOSCELES},
 
                 { "4 5 20", Triangle.NOT_TRIANGLE},
+                { "2 1 1", Triangle.NOT_TRIANGLE},
+                { "1 2 1", Triangle.NOT_TRIANGLE},
+                { "1 1 2", Triangle.NOT_TRIANGLE},
         };
     }
 }
